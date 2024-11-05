bd529d9dc6eb73765f79be1d5cc56f10dde17d34
isakibul <isakibul108@gmail.com>
2024-11-05 15:50:30 +0600

    fundamental

---
+++ app.py
@@ -0,0 +1,71 @@
+# print("Hello World!")
+
+
+
+# number1, number2, name = 24, 22, "Sakibul Islam"
+# print(number1, number2, name)
+
+
+
+# Literals
+# Literal are used to input fixed value in program
+# name = "Sakibul Islam"
+# Here name is a variable and "Sakibul Islam" is a literal
+
+
+
+# Type conversion
+
+# There are two type of conversion:
+
+# Implicit conversion - automatic type conversion:
+# int_num = 24
+# float_num = 10.50
+# sum = int_num+float_num
+# print(sum)
+# print(type(sum))
+
+# Explicit conversion(Type casting) - manual type conversion:
+# num_str = "10"
+# num_int = 24
+# print(type(num_int))
+# num_str = int(num_str) - we can also use float(), str()
+# add = num_str+num_int
+# print(add)
+
+
+
+# Python print with end() parameter
+# print('Good Morning!', end= ' ')
+
+# Python print with sep() parameter
+# print('New Year', 2024, 'See you soon!', sep= '. ')
+
+# Print concatenate string
+# print("Sakibul" + " " + "Islam")
+
+# Output formatting
+# x=10
+# y=20
+# print('The value of x is {} and y is {}'.format(x,y))
+
+# Python input
+# name = input("Enter your name: ")
+# print(name)
+
+
+
+# Python operators
+# Floor division - // - 10//3=9
+# Power - ** - 4**2=16
+# Exponent Assignment - **= - x**=3 - Output: 8
+
+# Identify operator
+# x1=5
+# y1=5
+# print(x1 is not y1)
+
+# Membership operator
+# message="Hello World"
+# print("Hello" in message)
+# print("World" not in message)
